21d6b37231a30bb5978e418681ec7321a85afc4c
duke <1181249332@qq.com>
2019-04-23 18:17:36 +0800

    更新

--- src/test/java/priv/gitonlie/flowable/SpringbootFlowableApplicationTests.java
+++ src/test/java/priv/gitonlie/flowable/SpringbootFlowableApplicationTests.java
@@ -1,16 +1,33 @@
 package priv.gitonlie.flowable;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import priv.gitonlie.flowable.process.Demo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootFlowableApplicationTests {
 
+	@Autowired
+	private Demo demo;
+	@Autowired
+	SqlSession sqlSession;
 	@Test
 	public void contextLoads() {
+//		demo.startUp();
+//		demo.completeTask("2505", "小明");
+		Map<String,Object> map = new HashMap<String, Object>();
+		map.put("assignee", "dasdasd");
+		map.put("actId", "startevent1");
+		sqlSession.update("updateAct",map);
 	}
 
 }
